538984e7702edc2accd8c7bb1f7f3d8ea3632c88
LemyreG <LemyreG@github.com>
2017-12-06 17:04:18 -0500

    Better help calls. Less useless prints.

--- R_LaTeX_Tabular.R
+++ R_LaTeX_Tabular.R
@@ -1,24 +1,8 @@
-chr <- function(n) { rawToChar(as.raw(n)) }
-
-Copie.Presse.Papier <- function(string) {
-	os <- Sys.info()[['sysname']]
-	if (os == "Windows") { # Si systeme dexploitation windows
-		return(utils::writeClipboard(string))
-	} else if (os == "Darwin") { # Si systeme dexploitation iOS
-		Mac.Copie.Presse.Papier <- function(string){
-			presse.papier <- pipe("pbcopy", "w")
-			cat(string, file = presse.papier, sep = "\n")
-			close(presse.papier)	# Fermer lobjet presse-papier
-		}
-		return(Mac.Copie.Presse.Papier(string))
-	}
-}
-
 Make.LaTeX.Table = function(R.Matrix.Object,
 							caption=TRUE,
-                            title = "", 
-						    Col.Titles = NULL, 
-    						Row.Titles = NULL,
+							title = "",
+							Col.Titles = NULL,
+							Row.Titles = NULL,
 							n.dec = 3,
 							type = "Table",
 							Row.Pos="c",
@@ -125,65 +109,90 @@ cat("---------------------------------------------------------------------------
 cat("This package was created by [ Gabriel Lemyre ] in Montreal, Qc, Canada \n")
 cat(paste("Last updated on the {",format(Sys.Date(), format="%B %d, %Y"),"} \n",sep=""))
 cat("------------------------------------------------------------------------------------------------------------- \n")
-cat(paste("For the code to run in LaTeX, your TeX editor requires the package ",chr(92),"usepackage{caption} if caption=TRUE \n",sep=""))
-cat("------------------------------------------------------------------------------------------------------------- \n")
-cat("Prints the LaTeX code for a tabular environnement from an R matrix object \n",
-	"[ Make.LaTeX.Table ] \n",
-    "R.Matrix.Object,    [matrix]  The object to be printed in LaTeX format", 
-	"\n caption=TRUE,       [boolean] Should a caption be printed",
-    "\n title='',           [string]  That title of the tabular environnement",
-    "\n Col.Titles=NULL,    [vector]  The titles of the columns, must be at least the same lenght as ncol(R.Matrix.Object)-1 and at most ncol(R.Matrix.Object)",
-    "\n Row.Titles=NULL,    [vector]  The titles of the rows, must be exactly the same length as nrow(R.Matrix.Object)",
-    "\n n.dec = 3,          [scalar]  The number of decimals to round the numeric values in the table",
-    "\n type='Table',       [string]  The type to table to be created, 'ConfInt' refers to confidence intervals, R.Matrix.Object must have exactly 2 columns",
-    "\n Row.Pos='c',        [string]  The position of the columns (Not including the title of rows which are right-justified)",
-	"\n Cross.Lines=FALSE,  [boolean] Should lines be printed under first row and first column",
-	"\n print.Cons=TRUE,    [boolean] Should the output be printed in the console",
-	"\n copy.CB=TRUE        [boolean] Should the output be copied to the users clipboard",
-    "\n")
-cat("--------------------------------------------------- \n")
-cat("Note that if the matrix has only one line, the horizontale line won't appear even if Cross.Lines=TRUE \n")
-cat("------------------------------------------------------------------------------------------------------------- \n")
-cat("The printed result is also copied to the users clipboard. \n")
-cat("------------------------------------------------------------------------------------------------------------- \n")
-cat("For help, contact gabriel @ lemyre . org. \n")
+cat(paste("For help, write \n",
+	"help.tabMaker() in the console for parameters \n",
+	"help.tabMaker.Example() for an example of usage. \n",
+	"If your have questions, contact gabriel @ lemyre . org. \n",sep=""))
 cat("------------------------------------------------------------------------------------------------------------- \n \n")
-cat("Both the following INPUTS \n")
-cat('% ------------------------ \n',
-	'Matrix.Object = matrix(c(seq(1,6)),ncol=3, byrow=TRUE) \n',
-	'col.Titles = c("","Column title 1","Column title 2","Column title 3") \n',
-	'line.Titles = c("Row title 1","Row title 2") \n',
-	'\n',
-	'Make.LaTeX.Table(Matrix.Object, title="Table Title", Col.Titles=col.Titles, Row.Titles=line.Titles) # Creation de la table LaTeX \n',
-	'% ------------------------ \n')
-cat('AND \n')
-cat('% ------------------------ \n',
-	'Matrix.Object = matrix(c(seq(1,6)),ncol=3, byrow=TRUE) \n',
-	'col.Titles = c("","Column title 1","Column title 2","Column title 3") \n',
-	'line.Titles = c("Row title 1","Row title 2") \n',
-	'\n',
-	'Matrix.Object.2 = rbind(col.Titles,cbind(line.Titles,Matrix.Object)) \n',
-	'Make.LaTeX.Table(Matrix.Object.2, title="Table Title", Col.Titles=col.Titles, Row.Titles=line.Titles) # Creation de la table LaTeX \n',
-	'% ------------------------ \n')
-cat("------------------------------------------------------------------------------------------------------------- \n")
-cat("gives the following OUTPUT \n")
-cat(paste("% ------------------------ \n", 
-	chr(92),"begin{minipage}{",
-	chr(92),"linewidth} \n",
-	chr(92),"centering",
-	chr(92),"captionof{table}{Table Title} \n",
-	chr(92),"label{Table Title} \n",
-	chr(92),"begin{tabular}[t]{r | *3{c} } \n",
-    chr(38)," Column title 1 ",chr(38)," Column title 2 ",chr(38)," Column title 3 ",chr(92),chr(92)," \n",
-	chr(92),"hline \n",
- 			"Row title 1 ",chr(38)," $1$ ",chr(38)," $2$ ",chr(38)," $3$  ",chr(92),chr(92)," \n",
- 			"Row title 2 ",chr(38)," $4$ ",chr(38)," $5$ ",chr(38)," $6$  ",chr(92),chr(92)," \n",
-	chr(92),"end{tabular} \n",
-	chr(92),"end{minipage} \n",
- 			"~",chr(92),chr(92)," \n",
- 			"% ------------------------ \n",sep=""))
+
+help.tabMaker = function(){
+	cat(paste("For the code to run in LaTeX, your TeX editor requires the package ",chr(92),"usepackage{caption} if caption=TRUE \n",sep=""))
+	cat("------------------------------------------------------------------------------------------------------------- \n")
+	cat(paste("Prints the LaTeX code for a tabular environnement from an R matrix object \n",
+		"[ Make.LaTeX.Table ] \n",
+		"R.Matrix.Object,    [matrix]  The object to be printed in LaTeX format", 
+		"\n caption=TRUE,       [boolean] Should a caption be printed",
+		"\n title='',           [string]  That title of the tabular environnement",
+		"\n Col.Titles=NULL,    [vector]  The titles of the columns, must be at least the same lenght as ncol(R.Matrix.Object)-1 and at most ncol(R.Matrix.Object)",
+		"\n Row.Titles=NULL,    [vector]  The titles of the rows, must be exactly the same length as nrow(R.Matrix.Object)",
+		"\n n.dec = 3,          [scalar]  The number of decimals to round the numeric values in the table",
+		"\n type='Table',       [string]  The type to table to be created, 'ConfInt' refers to confidence intervals, R.Matrix.Object must have exactly 2 columns",
+		"\n Row.Pos='c',        [string]  The position of the columns (Not including the title of rows which are right-justified)",
+		"\n Cross.Lines=FALSE,  [boolean] Should lines be printed under first row and first column",
+		"\n print.Cons=TRUE,    [boolean] Should the output be printed in the console",
+		"\n copy.CB=TRUE        [boolean] Should the output be copied to the users clipboard",
+		"\n",sep=""))
+	cat("--------------------------------------------------- \n")
+	cat("Note that if the matrix has only one line, the horizontale line won't appear even if Cross.Lines=TRUE \n")
+	cat("------------------------------------------------------------------------------------------------------------- \n")
+	cat("The printed result is also copied to the users clipboard. \n")
+	cat("------------------------------------------------------------------------------------------------------------- \n")
+}
+
+help.tabMaker.Example = function(){
+	cat("Both the following INPUTS \n")
+	cat('% ------------------------ \n',
+		'Matrix.Object = matrix(c(seq(1,6)),ncol=3, byrow=TRUE) \n',
+		'col.Titles = c("","Column title 1","Column title 2","Column title 3") \n',
+		'line.Titles = c("Row title 1","Row title 2") \n',
+		'\n',
+		'Make.LaTeX.Table(Matrix.Object, title="Table Title", Col.Titles=col.Titles, Row.Titles=line.Titles) # Creation de la table LaTeX \n',
+		'% ------------------------ \n')
+	cat('AND \n')
+	cat('% ------------------------ \n',
+		'Matrix.Object = matrix(c(seq(1,6)),ncol=3, byrow=TRUE) \n',
+		'col.Titles = c("","Column title 1","Column title 2","Column title 3") \n',
+		'line.Titles = c("Row title 1","Row title 2") \n',
+		'\n',
+		'Matrix.Object.2 = rbind(col.Titles,cbind(line.Titles,Matrix.Object)) \n',
+		'Make.LaTeX.Table(Matrix.Object.2, title="Table Title", Col.Titles=col.Titles, Row.Titles=line.Titles) # Creation de la table LaTeX \n',
+		'% ------------------------ \n')
+	cat("------------------------------------------------------------------------------------------------------------- \n")
+	cat("gives the following OUTPUT \n")
+	cat(paste("% ------------------------ \n", 
+			  chr(92),"begin{minipage}{",
+			  chr(92),"linewidth} \n",
+			  chr(92),"centering",
+			  chr(92),"captionof{table}{Table Title} \n",
+			  chr(92),"label{Table Title} \n",
+			  chr(92),"begin{tabular}[t]{r | *3{c} } \n",
+			  chr(38)," Column title 1 ",chr(38)," Column title 2 ",chr(38)," Column title 3 ",chr(92),chr(92)," \n",
+			  chr(92),"hline \n",
+			  "Row title 1 ",chr(38)," $1$ ",chr(38)," $2$ ",chr(38)," $3$  ",chr(92),chr(92)," \n",
+			  "Row title 2 ",chr(38)," $4$ ",chr(38)," $5$ ",chr(38)," $6$  ",chr(92),chr(92)," \n",
+			  chr(92),"end{tabular} \n",
+			  chr(92),"end{minipage} \n",
+			  "~",chr(92),chr(92)," \n",
+			  "% ------------------------ \n",sep=""))
+}
+
 	
 	
+chr <- function(n) { rawToChar(as.raw(n)) }
+
+Copie.Presse.Papier <- function(string) {
+	os <- Sys.info()[['sysname']]
+	if (os == "Windows") { # Si systeme dexploitation windows
+		return(utils::writeClipboard(string))
+	} else if (os == "Darwin") { # Si systeme dexploitation iOS
+		Mac.Copie.Presse.Papier <- function(string){
+			presse.papier <- pipe("pbcopy", "w")
+			cat(string, file = presse.papier, sep = "\n")
+			close(presse.papier)	# Fermer lobjet presse-papier
+		}
+		return(Mac.Copie.Presse.Papier(string))
+	}
+}
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
